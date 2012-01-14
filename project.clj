29304f151f1807f6d50424aac956008cc0bcaea8
Aaron Crow <dirtyvagabond@yahoo.com>
2012-01-14 14:44:18 -0800

    up funnyplaces dep to 1.2.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject pretty-ql "1.0.1"
   :description "An SQL-ish Clojure DSL for Factual's API"
   :dependencies [[org.clojure/clojure "1.4.0-alpha2"]
-                 [funnyplaces "1.2.0"]]
+                 [funnyplaces "1.2.1"]]
   :dev-dependencies [
     [swank-clojure "1.4.0-SNAPSHOT"]
     [lein-clojars "0.6.0"]
