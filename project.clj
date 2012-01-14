53522464b89699b45b9217c21430dee545d92995
Aaron Crow <dirtyvagabond@yahoo.com>
2012-01-14 14:45:03 -0800

    up version to 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pretty-ql "1.0.1"
+(defproject pretty-ql "1.0.2"
   :description "An SQL-ish Clojure DSL for Factual's API"
   :dependencies [[org.clojure/clojure "1.4.0-alpha2"]
                  [funnyplaces "1.2.1"]]
