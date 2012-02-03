9ae0b68cf5020e2d7edf0df6212ba99b29657b16
Aaron Crow <dirtyvagabond@yahoo.com>
2012-02-02 20:03:21 -0800

    misc. cleanup project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,4 @@
   :description "An SQL-ish Clojure DSL for Factual's API"
   :dependencies [[org.clojure/clojure "1.4.0-alpha2"]
                  [funnyplaces "1.2.1"]]
-  :dev-dependencies [
-    [swank-clojure "1.4.0-SNAPSHOT"]
-    [lein-clojars "0.6.0"]
-  ])
+  :dev-dependencies [[lein-clojars "0.6.0"]])
